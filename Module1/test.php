ffd6635796d78546b7dab64b60fc2d99183bc151
Selvi <s.dhandapani@outlook.com>
2017-02-24 09:29:50 -0500

    Some changes

--- Module1/test.php
+++ Module1/test.php
@@ -1,1 +1,34 @@
+<!doctype html>
+<html>
+	<head>
+	<title>FirstApplication</title>
+		<link rel='stylesheet' href='main.css'>
+	</head>
+	<body>
+	
+		<h1>My First Application</h1>
+		<p>Welcome to <i>learning Javascript 3rd Edition</i>.</p>
+		<button id="button1"></button>
+		<button id="button2" onclick=hello()>button2</button>
+		<!--  <canvas id='mainCanvas'></canvas>-->
+		
+		
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js">
+		</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.10.2/paper-full.min.js">
+		</script>
+		<script type="text/javascript" src='main.js'>
+	</script>
+
+<script>
+
+	button1.onclick = test;
+	function test(){
+		alert("hello2");
+		document.getElementById("button1").style.backgroundColor="green";
+	}
+
+</script>
 <?php phpinfo();?>
+	</body>
+</html>
